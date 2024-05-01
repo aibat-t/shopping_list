e7cdb1a892a5356b8b8f02036bf3a91017d32703
Tozhybayev Aibat <a.tozhybayev@alantech.kz>
2024-05-01 17:07:00 +0500

    added form fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:shopping_list/screen/grocery_list.dart';
+import 'package:shopping_list/widget/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: const GroceryListScreen(),
+      home: const GroceryList(),
     );
   }
 }
--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widget/new_item.dart';
 
-class GroceryListScreen extends StatelessWidget {
-  const GroceryListScreen({
-    super.key,
-  });
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
